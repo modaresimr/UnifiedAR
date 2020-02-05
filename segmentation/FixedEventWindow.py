2c1997a04fc239ed08ecc215758c647bba2f9937
alim1369 <alim1369@gmail.com>
2020-02-05 21:29:51 +0100

    m

--- segmentation/FixedEventWindow.py
+++ segmentation/FixedEventWindow.py
@@ -38,3 +38,29 @@ class FixedEventWindow(Segmentation):
         window.iat[0, 1].value
         return {'window': window, 'start': stime, 'end': etime}
 
+    def segment2(self, w_history, buffer):
+        shift = self.shift
+        size = self.size
+
+        if len(w_history) == 0:
+            lastStart = pd.to_datetime(0)
+        else:
+            lastStart = buffer.times[w_history[len(w_history)-1][0]]
+
+        sindex = buffer.searchTime(lastStart, -1)
+
+        if(sindex is None):
+            return None
+        sindex = sindex+shift
+        if(len(buffer.times) <= sindex):
+            return None
+
+        eindex = min(len(buffer.times)-1, sindex+size)
+        if(eindex-sindex < size):
+            return None
+        # etime = buffer.times[eindex]
+        # stime = buffer.times[sindex]
+        idx = range(sindex, eindex + 1)
+        # buffer.removeTop(sindex)
+
+        return idx
