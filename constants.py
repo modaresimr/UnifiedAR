54ff85c70eddde9f270ccce240aa5ea94373d2f3
alim1369 <alim1369@gmail.com>
2020-05-09 13:53:20 +0200

    d

--- constants.py
+++ constants.py
@@ -26,6 +26,7 @@ from metric import *
 import ml_strategy.Simple
 import ml_strategy.SeperateGroup
 import ml_strategy.WeightedGroup
+import ml_strategy.WeightedGroup2
 import preprocessing.SimplePreprocessing
 import segmentation.Probabilistic
 import segmentation.FixedEventWindow
@@ -39,11 +40,11 @@ methods.segmentation = [
    {'method': lambda: segmentation.FixedEventWindow.FixedEventWindow(), 'params': [
        {'var': 'size', 'min': 10, 'max': 30, 'type': 'int', 'init': 14, 'range':list(range(15,30,4))},
        {'var': 'shift', 'min': 2, 'max': 20, 'type': 'int', 'init': 12, 'range':list(range(10,30,5))}
-          ], 'findopt': True},
+          ], 'findopt': False},
     {'method': lambda: segmentation.FixedSlidingWindow.FixedSlidingWindow(), 'params': [
         {'var': 'size' , 'min': 60, 'max': 15*60, 'type': 'float', 'init': 120/2, 'range':list(range(30,180,60))},
         {'var': 'shift', 'min': 10, 'max': 7*60 , 'type': 'float', 'init': 60, 'range':list(range(30,180,60))}
-    ], 'findopt': True}
+    ], 'findopt': False}
     #   {'method': lambda:segmentation.FixedTimeWindow.FixedTimeWindow(), 'params':[
     #                  {'var':'size','min':pd.Timedelta(1, unit='s').total_seconds(), 'max': pd.Timedelta(30, unit='m').total_seconds(), 'type':'float','init':pd.Timedelta(15, unit='s').total_seconds()},
     #                  {'var':'shift','min':pd.Timedelta(1, unit='s').total_seconds(), 'max': pd.Timedelta(30, unit='m').total_seconds(), 'type':'float','init':pd.Timedelta(1, unit='s').total_seconds()}
@@ -145,6 +146,7 @@ methods.mlstrategy = [
     #{'method': lambda: ml_strategy.Simple.SimpleStrategy()},
     # {'method': lambda: ml_strategy.SeperateGroup.SeperateGroupStrategy()},
     {'method': lambda: ml_strategy.WeightedGroup.WeightedGroupStrategy(alpha=20)},
+       {'method': lambda: ml_strategy.WeightedGroup2.WeightedGroup2Strategy(alpha=20)},
     
 ]
 
